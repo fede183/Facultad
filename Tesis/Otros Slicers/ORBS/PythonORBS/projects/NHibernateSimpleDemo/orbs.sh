3e88ea80f0834d4037ab054f53ebbe65ed116e98
fede183 <fede.183@hotmail.com>
2018-10-11 14:39:14 -0300

    Agregado PythonORBS

---
+++ Tesis/Otros Slicers/ORBS/PythonORBS/projects/NHibernateSimpleDemo/orbs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This file is part of the ORBS distribution.
+# See the file LICENSE.TXT for more information.
+
+# Invoke ORBS.
+
+# Configure ORBS for this project.
+source config/config.sh
+
+# Pass the arguments to ORBS together with the list of files to be sliced.
+python3  "$ORBS_SCRIPT" $* $ORBS_FILES
+
